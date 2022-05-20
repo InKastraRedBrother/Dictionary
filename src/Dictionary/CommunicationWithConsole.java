e37d32586a6a9664032d085e9eccae55e0c37854
InKastraRedBrother <avpeterimov@mail.ru>
2022-05-20 19:08:00 +0300

    small changes in ConWithCon class

--- src/Dictionary/CommunicationWithConsole.java
+++ src/Dictionary/CommunicationWithConsole.java
@@ -4,13 +4,19 @@ import java.io.Console;
 import java.util.Scanner;
 
 public class CommunicationWithConsole {
-    //String s;  //if s will be initialized in InputInConsole method , then it will be "" in some circumstances (1st input "" (just Enter) 2nd input matches(1 or 2))
+    String s;  //if s will be declared in InputInConsole method , then it will be "" in some circumstances (1st input "" (just Enter) 2nd input matches(1 or 2))
 
     public CommunicationWithConsole() {
     }
 
-    String s;
-
+    /** Output message for user. Chooses valid console to input String.
+     * Validate input string.
+     * Method uses recursion for checking input string
+     *
+     * @param selectionType get String which will be printed
+     * @param pattern check if input String matches geted pattern
+     * @return Valid String from valid console input
+     */
     public String inputInConsole(String selectionType, String pattern) {
         System.out.println(selectionType);
         s = consoleChooser();
